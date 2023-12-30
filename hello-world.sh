00d3b07780a029a9dbcbe3efff7d0def92ad2636
Arunponugotii <arunponugoti2565@gmail.com>
2023-12-30 12:46:16 +0530

    new change

--- hello-world.sh
+++ hello-world.sh
@@ -3,6 +3,8 @@ DATE=$(date)
 echo "this script is running at a time of: $DATE"
 echo "hi, my name is tharun"
 echo "im going to be master in devops"
-p1=$1
-p2=$2
-echo "$1 and $p2 are useless"
+echo "enter your numbers"
+read p1
+read p2
+sum = $(($p1+$p2))
+echo "entered nums $p1 ,$p2 and sum is :$sum"

62e6ab54895ca9e5bda52ca3a9b9195e925d50fc
Arunponugotii <arunponugoti2565@gmail.com>
2023-12-30 16:31:25 +0530

    new changes

--- conditions.sh
+++ conditions.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+VALIDATE(){
+    #$1 is argument you give
+    if [ $1 -name 0 ]
+    then
+        echo "Installion $2 Failure"
+        exit 1
+    else
+        echo "Installation $2 Success"
+    fi
+}
+
+
+
 NUM=$(id -u)
 if [ $NUM -ne 0 ]
 then
-    echo "you are not root user"
-else
-    echo "you are root user"
-    yum install postfix -y
+    echo "Error: you are not root user."
+    exit 1
 fi
+
+yum install mysql -y
+VALIDATE $? mysql
+yum install postfix -y
+VALIDATE $? postfix

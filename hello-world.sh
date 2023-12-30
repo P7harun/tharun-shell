0d3e8a80d55524246dfdd80babf7334a116e6b78
Arunponugotii <arunponugoti2565@gmail.com>
2023-12-30 12:38:01 +0530

    new change

--- hello-world.sh
+++ hello-world.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DATE=$(date)
-echo"this script is running at a time of: $DATE"
+echo "this script is running at a time of: $DATE"
 echo "hi, my name is tharun"
 echo "im going to be master in devops"
 p1=$1

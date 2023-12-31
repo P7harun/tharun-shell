becc0fd661cedf57f53cfa7c8bda713660d985dd
Arunponugotii <arunponugoti2565@gmail.com>
2023-12-31 10:50:50 +0530

    gmail added

--- gmail.sh
+++ gmail.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-USERID=(id -u)
-if [ $USERID -ne 0 ]
-then
-    sudo su -
-fi
-yum update -y --exclude=kernel*
-yum -y install postfix cyrus-sasl-plain mailx
-systemctl restart postfix 
-systemctl enable postfix
+# USERID=(id -u)
+# if [ $USERID -ne 0 ]
+# then
+#     sudo su -
+# fi
+# yum update -y --exclude=kernel*
+# yum -y install postfix cyrus-sasl-plain mailx
+# systemctl restart postfix 
+# systemctl enable postfix
 cd /etc/postfix/
 # D=$(relayhost = [smtp.gmail.com]:587 smtp_use_tls = yes smtp_sasl_auth_enable = yes smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd smtp_sasl_security_options = noanonymous smtp_sasl_tls_security_options = noanonymous)
 # sed -e '$ a $D' main.cf

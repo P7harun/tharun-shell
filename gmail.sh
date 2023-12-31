f35712ff1268bc2edf40dc436d6e26c1af7acf4b
Arunponugotii <arunponugoti2565@gmail.com>
2023-12-31 10:35:27 +0530

    gmail added

--- gmail.sh
+++ gmail.sh
@@ -5,24 +5,19 @@ then
     sudo su -
 fi
 yum update -y --exclude=kernel*
-yum -y install postfix cyrus-sasl-plain mailx
-systemctl restart postfix 
-systemctl enable postfix
-cd /etc/postfix/
-D=$(relayhost = [smtp.gmail.com]:587
-smtp_use_tls = yes
-smtp_sasl_auth_enable = yes
-smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
-smtp_sasl_security_options = noanonymous
-smtp_sasl_tls_security_options = noanonymous)
-sed -e '$ a $D' main.cf
-touch /etc/postfix/sasl_passwd  
-cd /etc/postfix/
-echo "enter username"
-read USERNAME
-echo "enter apppassword"
-read APASWORD
-sed -e '$ a [smtp.gmail.com]:587 $USERNAME:APASWORD' sasl_passwd
-postmap sasl_passwd 
+# yum -y install postfix cyrus-sasl-plain mailx
+# systemctl restart postfix 
+# systemctl enable postfix
+# cd /etc/postfix/
+# D=$(relayhost = [smtp.gmail.com]:587 smtp_use_tls = yes smtp_sasl_auth_enable = yes smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd smtp_sasl_security_options = noanonymous smtp_sasl_tls_security_options = noanonymous)
+# sed -e '$ a $D' main.cf
+# touch /etc/postfix/sasl_passwd  
+# cd /etc/postfix/
+# echo "enter username"
+# read USERNAME
+# echo "enter apppassword"
+# read APASWORD
+# sed -e '$ a [smtp.gmail.com]:587 $USERNAME:APASWORD' sasl_passwd
+# postmap sasl_passwd 
 
-echo "This is a test mail & Date $(date)" | mail -s "gmail shell script is done" tharun186189@gmail.com
+# echo "This is a test mail & Date $(date)" | mail -s "gmail shell script is done" tharun186189@gmail.com
